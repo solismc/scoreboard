a46dbe7d1f6a38ca06176b1bb7f5e51864a417e8
Manuel C Solis <manuelsolis@Manuels-MacBook-Air.local>
2018-07-31 15:19:36 -0400

    updated the functions to update team name

--- public/main.js
+++ public/main.js
@@ -1,16 +1,34 @@
 
+const main = () => {
+  document.querySelector('h1').textContent += '?'
+}
+
+document.addEventListener('DOMContentLoaded', main)
+
+
 const updateTeamTwoName=()=> {
   console.log('updated')
   console.log('button was clicked')
 }
 
 
+const buttonClickEvent = () => {
+  console.log('button was clicked')
+  const newTeamName = document.querySelector('new-home-team').value
+  document.querySelector('.team1').textContent = newTeamName
+}
+
+document.addEventListener('DOMContentLoaded', main)
+document.querySelector('.home-team-update').addEventListener('click', buttonClickEvent)
+document.querySelector('.away-team-update').addEventListener('click', buttonClickEvent)
+
+
+
+
+document.querySelector('.update-team-one-button').addEventListener('click',updateTeamOneName)
+document.querySelector('.output1').textContent
 document.querySelector('.update-team-two-button').addEventListener('click',updateTeamTwoName)
 
 const updateTeamOneName=()=> {
   console.log('updated')
 }
-
-document.querySelector('.update-team-one-button').addEventListener('click',updateTeamOneName)
-
-document.querySelector('.output1').textContent
